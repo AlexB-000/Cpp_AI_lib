dee96aea517fd6aa320c1b63080457983a36284c
AlexB-000 <alexborissov1@gmail.com>
2026-02-22 10:33:07 +0100

    feat: main include

---
+++ Cpp_AI.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "include/preprocessing/train_test_split.hpp"
+#include "include/evalutaion.hpp"
+
+#include "include/deep_learning/network.hpp"
+#include "include/deep_learning/linear.hpp"
+#include "include/deep_learning/ReLU.hpp"
+#include "include/deep_learning/tanh.hpp"
+
+#include "include/optimizers/GD.hpp"
+#include "include/loss/MSE_Loss.hpp"
+
+#include "include/DataLoader.hpp"
+#include "include/Dataset.hpp"
